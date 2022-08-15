00f2f4eb6c4e8fba3623c164073b98d48f406efa
lipollis <aline.pollis@hotmail.com>
2022-08-15 17:40:58 -0300

    baseUrl localhost

--- frontend/src/app/config/api.config.ts
+++ frontend/src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-   //baseUrl : 'https://helpdesk-lipollis.herokuapp.com'
+   // baseUrl : 'https://helpdesk-lipollis.herokuapp.com/'
    baseUrl : 'http://localhost:8080'
 }
